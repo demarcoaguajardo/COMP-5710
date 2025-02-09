435259b1312c8821ee0feaf4294adaa7c3319e41
Demarco Guajardo <124603625+demarcoaguajardo@users.noreply.github.com>
2025-02-09 13:18:10 -0600

    Update test-calculator.py to include test failures

--- W01 - Test-Driven Development/test-calculator.py
+++ W01 - Test-Driven Development/test-calculator.py
@@ -8,6 +8,8 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(calculator.multiply(0, 4), 0)
         self.assertEqual(calculator.multiply(0, -29), 0)
         self.assertEqual(calculator.multiply(100, 2), 200)
+        # Example of test case failure
+        self.assertEqual(calculator.multiply(2, 3), 7)
 
     def test_divide(self):
         self.assertEqual(calculator.divide(10, 2), 5)
@@ -17,6 +19,8 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(calculator.divide(100, 2), 50)
         with self.assertRaises(ValueError):
             calculator.divide(5, 0)
+        # Example of test case failure
+        self.assertEqual(calculator.divide(10, 2), 4)
 
     def test_sqrt(self):
         self.assertEqual(calculator.sqrt(4), 2)
@@ -25,6 +29,8 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(calculator.sqrt(25), 5)
         with self.assertRaises(ValueError):
             calculator.sqrt(-1)
+        # Example of test case failure
+        self.assertEqual(calculator.sqrt(4), 3)
 
 if __name__ == '__main__':
     unittest.main()
